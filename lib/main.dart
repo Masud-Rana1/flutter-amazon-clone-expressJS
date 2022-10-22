f00f81ea2ef8abfc56d0beb0930b9d2b2bab644b
masud-rana-tech <developer.rana99@gmail.com>
2022-10-22 22:53:31 +0600

    persisting-state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-
 import 'package:amazon_clone_f/constants/global_variables.dart';
 import 'package:amazon_clone_f/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone_f/features/auth/services/auth_service.dart';
+import 'package:amazon_clone_f/features/home/screens/home_screen.dart';
 import 'package:amazon_clone_f/providers/user_provider.dart';
 import 'package:amazon_clone_f/router.dart';
 import 'package:flutter/material.dart';
@@ -8,13 +9,26 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (context)=> UserProvider())
-  ],child: const MyApp()));
+  runApp(MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => UserProvider())],
+      child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context: context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +45,7 @@ class MyApp extends StatelessWidget {
               elevation: 0,
               iconTheme: IconThemeData(color: Colors.black))),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home:const AuthScreen(),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty ? const HomeScreen() : const AuthScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const expresss = require('express');
 const User = require('../models/user');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const auth = require('../middlewares/auth');
 
 const authRouter = expresss.Router();
  authRouter.post("/api/singup", async (req, res) =>{
@@ -33,7 +34,7 @@ const authRouter = expresss.Router();
  //Exercise
  authRouter.post("/api/signin", async (req,res)=>{
    try{
-      const {name, email, password} = req.body;
+      const { email, password} = req.body;
       const user = await User.findOne({email});
       if(!user){
          return res.status(400).json({msg: "user with this email  does not exists!"});
@@ -47,6 +48,27 @@ const authRouter = expresss.Router();
    }catch(e){
       res.status(500).json({error: e.message}); 
      }
+ });
+
+ authRouter.post("/tokenIsValid", async (req,res)=>{
+   try{
+      const token = req.header('x-auth-token');
+      if(!token) return res.json(false);
+      const verified = jwt.verify(token,"passwordKey");
+      if(!verified) return res.json(false);
+      const user = await User.findById(verified.id);
+      if(!user) return res.json(false);
+      res.json(true);
+
+   }catch(e){
+      res.status(500).json({error: e.message});
+   }
+ });
+
+ //get user data api
+ authRouter.get('/', auth, async(req,res)=>{
+   const user = await User.findById(req.user);
+   res.json({...user._doc, token: req.token});
  });
 
  module.exports = authRouter;
